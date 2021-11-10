0f6104b67a749583937de49c5720fa02299b06dd
amuname <74045707+amuname@users.noreply.github.com>
2021-11-11 01:45:05 +0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,8 @@ data:function(){
 		// {id:new Date().value,text:'user text'},
 		// ],
 		user_text:'',
-
+		search_result:[],
+		search_result_show:false,
 	}
 },
 created:function () {
@@ -19,29 +20,64 @@ methods:{
 	add_to_list:function(item){
 		this.user_list.push(item)
 		this.user_text=''
+		this.search_result = []
 	},
 	remove_from_list:function(list_id){
 		this.user_list = this.user_list.filter(elem=>elem.id!=list_id)
 	},
+	show_result:async function(){
+		if (this.user_text!='') {
+			const res = await fetch('/fakedata/search.json')
+			const json = await res.json() 
+			const s_list = json
+			this.search_result = s_list.filter(
+				elem=>elem.text.startsWith(this.user_text) 
+				) 
+			if(this.search_result.length>0) this.search_result_show = true
+
+
+		} else {
+			this.search_result_show = false
+		}
+	},
+	choosen_search_result:function (item) {
+		this.user_text=item.text
+		this.search_result_show = false
+	},
 },
+props:['title'],
 template:`
 <div>
+	<p> {{ title }} </p>
 	<ol>
-		<li v-for="item in user_list" v-bind:key="item.id" v-bind:item="item">
-		<span> {{ item.text }} </span>
+		<li v-for="item in user_list" v-bind:key="item.id">
+		<span> {{ item.text }} 
 		<button v-on:click="
 		remove_from_list(item.id)
-		">X</button>
+		">X</button></span>
 		</li>
 	</ol>
-	<input v-model="user_text" type="text"/>
-	<button v-if="user_text!=''" v-on:click="
+	<form v-on:submit="(e)=>{
+	e.preventDefault()
 	add_to_list({id: new Date().valueOf(),text:user_text})
-	"> Add to List "{{user_text}}" </button>
+	}
+	">
+		<input v-model="user_text" v-on:input="show_result" type="text"/>
+		<ul v-if="search_result_show">
+			<li v-for="item in search_result" v-bind:key="item.id" v-on:click="
+			choosen_search_result(item)
+			">
+			 {{item.text}} 
+			</li>
+		</ul>
+		<button v-if="user_text!=''""
+		> Add to List "{{user_text}}" </button>
+	</form>
 </div>
 `
 })
 
+// TO DO add refs?? focus input after search result selected
 
 
 
@@ -49,7 +85,21 @@ template:`
 
 const app = new Vue({
 	el:'#app',
-	template:'<custom-list-component></custom-list-component>',
+	data:{
+		obj_name:'Test obj',
+		el_prop:{
+			customDataField:"Component description",
+			customListComponent:"Here list"
+		},
+	},
+	template:`
+	<div>
+	<custom-data-field v-bind:title="el_prop.customDataField">
+	</custom-data-field>
+	<custom-list-component v-bind:title="el_prop.customListComponent">
+	</custom-list-component>
+	</div>
+	`,
 })
 
 
